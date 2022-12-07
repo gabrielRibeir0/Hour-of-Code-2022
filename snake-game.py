4f8385e7b0f1d6c2f30affe33ddd3741874c8fc5
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2022-12-07 20:17:15 +0000

    Parte 2 - Snake Game

--- snake-game.py
+++ snake-game.py
@@ -1,6 +1,7 @@
 import pygame, random
-from pygame import *
+from pygame.locals import *
 
+#inicialização
 pygame.init()
 screen = pygame.display.set_mode((600,600))
 pygame.display.set_caption('Snake Game')
@@ -10,28 +11,33 @@ RIGHT = 1
 DOWN = 2
 LEFT = 3
 
+#gerar coordenadas aleatórias para a maçã
 def on_grid_random():
   x = random.randint(0,590)
   y = random.randint(0,590)
   return (x // 10 *10, y // 10 * 10)
 
+#função principal
 def game_loop():
   game_end = False
   
+  #inicialização da cobra e da maçã (posição, tamanho, cor, etc.)
   snake_direction = LEFT
   snake = [(200,200),(210,200),(220,200)]
   snake_skin = pygame.Surface((10,10))
   snake_skin.fill((255,127,80))
 
+  apple_pos = on_grid_random()
   apple = pygame.Surface((10,10))
   apple.fill((255,0,0))
-  apple_pos = on_grid_random()
   
+  #taxa de atualização do jogo
   clock = pygame.time.Clock()
   
   while not game_end:
     clock.tick(10)
 
+    #leitura dos eventos e alterar o movimento da cobra
     for event in pygame.event.get():
       if event.type == QUIT:
         game_end = True
@@ -46,10 +52,10 @@ def game_loop():
         elif event.key == K_RIGHT:
           snake_direction = RIGHT
     
-    #movimento da cobra
+    #movimento da cobra e da sua cabeça
     for i in range(len(snake) - 1, 0, -1):
       snake[i] = (snake[i-1][0], snake[i-1][1])
-
+    
     if snake_direction == UP:
       snake[0] = (snake[0][0],snake[0][1]-10)
     elif snake_direction == DOWN:
@@ -59,19 +65,24 @@ def game_loop():
     elif snake_direction == RIGHT:
       snake[0] = (snake[0][0] + 10, snake[0][1])
 
+    #verificar se a cobra comeu a maçã
     snake_head = snake[0]
+
     if snake_head[0] == apple_pos[0] and snake_head[1] == apple_pos[1]:
       apple_pos = on_grid_random()
       snake.append(snake[1])
 
+    #verificar se a cobra bateu em si mesma
     for i in range(1,len(snake)):
-      if snake_head[0] == snake[i][0] and snake_head[1] == snake[i][i]:
+      if snake_head[0] == snake[i][0] and snake_head[1] == snake[i][1]:
         game_end = True
         break
 
+    #verificar se a cobra saiu do mapa
     if snake_head[0] < 0 or snake_head[0] > 590 or snake_head[1] < 0 or snake_head[1] > 590:
       game_end = True
 
+    #desenhar o fundo, a maçã e a cobra
     screen.fill((0,0,0))
     screen.blit(apple,apple_pos)
 
@@ -82,4 +93,5 @@ def game_loop():
   
   pygame.quit()
 
-game_loop()
+if __name__ == '__main__':
+    game_loop()
